af6c125705d8128a898e7d74e1a343c96b620a0f
Christine Lin <lin.christine19@gmail.com>
2025-09-08 15:52:29 -0400

    add todo route

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask import (
     request,
     session,
     )
-
-from todos.utils import error_for_list_title
+from werkzeug.exceptions import NotFound
+from todos.utils import error_for_list_title, find_list_by_id, error_for_todo
 
 app = Flask(__name__)
 app.secret_key='secret1'
@@ -28,7 +28,7 @@ def index():
 def get_lists():
     return render_template('lists.html', lists=session['lists'])
 
-@app.route('/lists', methods=['POST'])
+@app.route("/lists", methods=["POST"])
 def create_list():
     title = request.form['list_title'].strip()
 
@@ -51,6 +51,34 @@ def create_list():
 def add_todo_list():
     return render_template('new_list.html')
 
+@app.route("/lists/<list_id>")
+def show_list(list_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound(description="List not found")
+    return render_template('list.html', lst=lst)
+
+@app.route("/lists/<list_id>/todos", methods=["POST"])
+def add_todo(list_id):
+    todo_title = request.form['todo'].strip()
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound(description="List not found")
+    
+    error = error_for_todo(todo_title)
+    if error:
+        flash(error, "error")
+        return render_template('list.html', lst=lst)
+    
+    lst.get('todos').append({
+        'id': str(uuid4()),
+        'title': todo_title,
+        'completed': False,
+    })
+
+    flash("The todo was added.", "success")
+    session.modified = True
+    return redirect(url_for('show_list', list_id=list_id))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
--- todos/utils.py
+++ todos/utils.py
@@ -5,3 +5,11 @@ def error_for_list_title(title, lists):
         return "The title must be between 1 and 100 characters."
     else:
         return None
+    
+def error_for_todo(title):
+    if not 1 <= len(title) <= 100:
+        return "The title must be between 1 and 100 characters"
+    return None
+    
+def find_list_by_id(list_id, lists):
+    return next((lst for lst in lists if lst.get('id') == list_id), None)
